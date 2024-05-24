11ac0911ac426a0ed719331e45d8298276304386
Imam Baihaqi <imambaihaq@students.unnes.ac.id>
2024-05-24 09:29:45 +0700

    add: Input dropdown for State

--- js.js
+++ js.js
@@ -1,20 +1,42 @@
-function dropdown() {
-    document.getElementById("Dropdown").classList.toggle("show");
-  }
-  
+document.getElementById('StateSearch').addEventListener('click', function(event) {
+  document.getElementById('StatedropdownList').classList.toggle('show');
+  event.stopPropagation(); // Prevent the event from bubbling up to the window click listener
+});
 
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) { 
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+function filterFunction() {
+  var input, filter, div, labels, i;
+  input = document.getElementById('StateSearch');
+  filter = input.value.toUpperCase();
+  div = document.getElementById('StatedropdownList');
+  labels = div.getElementsByTagName('label');
+  for (i = 0; i < labels.length; i++) {
+      txtValue = labels[i].textContent || labels[i].innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          labels[i].style.display = "";
+      } else {
+          labels[i].style.display = "none";
       }
-    }
+  }
 }
+
+// Prevent the dropdown from closing when clicking inside it
+document.getElementById('StatedropdownList').addEventListener('click', function(event) {
+  event.stopPropagation();
+});
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('#StateSearch')) {
+      var dropdowns = document.getElementsByClassName('state-dropdown-content');
+      for (var i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+              openDropdown.classList.remove('show');
+          }
+      }
+  }
+}
+
 
 function fetchJSONdata(){
     fetch("dataset.json")
